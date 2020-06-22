a7235f96cf2fdd1c88f9a3582fa415c47ae75655
Warren Harper II <warrenharper2@att.net>
2020-06-22 16:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function mapToNegativize(arr) {
 function mapToNoChange(arr) {
     return arr
 }
+
+function mapToDouble(arr) {
+    let newArr = []
+    for(let i = 0; i<arr.length; i++) {
+        newArr.push(arr[i]*2)
+    }
+    return newArr
+}
